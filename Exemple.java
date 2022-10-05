4c54562473f18a9349c83ce8e75f41fcad608e31
oussama-elbra <114907216+oussama-elbra@users.noreply.github.com>
2022-10-05 15:41:02 +0200

    Create Exemple.java

---
+++ Exemple.java
@@ -0,0 +1,12 @@
+package com.example ;
+import javax.swing.JOptionPane;
+
+public class Exemple {
+  public static void main (String[] args) {
+    //toto
+    system.out.println("hello word") ;
+    try {
+    }catch(Exception e){
+    }
+  }
+}
